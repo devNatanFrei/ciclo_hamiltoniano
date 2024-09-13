d44eb1a1ec9d8d78693d9c5d239a09a9c9a05e41
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-09-13 15:30:03 -0300

    adicionei algumas checagens

--- Ciclo.java
+++ Ciclo.java
@@ -8,7 +8,7 @@ public class Ciclo {
     private static final Map<String, List<String>> matrix_adj_2 = new HashMap<>();
     private static final Map<String, List<String>> matrix_adj_3 = new HashMap<>();
     private static final Map<String, List<String>> matrix_adj_4 = new HashMap<>();
-    public void setMatrix_adj_1(){
+    public static void setMatrix_adj_1(){
         matrix_adj_1.put("A", Arrays.asList("B", "C", "F", "G"));
         matrix_adj_1.put("B", Arrays.asList("A", "C"));
         matrix_adj_1.put("C", Arrays.asList("A", "B", "C","D"));
@@ -18,7 +18,7 @@ public class Ciclo {
         matrix_adj_1.put("G", Arrays.asList("A", "F"));
     }
 
-    public void setMatrix_adj_2(){
+    public static void setMatrix_adj_2(){
         matrix_adj_2.put("A", Arrays.asList("B", "C", "F", "G"));
         matrix_adj_2.put("B", Arrays.asList("A", "C"));
         matrix_adj_2.put("C", Arrays.asList("A", "B", "C","D"));
@@ -27,7 +27,7 @@ public class Ciclo {
         matrix_adj_2.put("F", Arrays.asList("A", "E","G"));
         matrix_adj_2.put("G", Arrays.asList("A","E" ,"F"));
     }
-    public void setMatrix_adj_3(){
+    public static void setMatrix_adj_3(){
         matrix_adj_3.put("A", Arrays.asList("B", "C", "F", "G"));
         matrix_adj_3.put("B", Arrays.asList("A", "C","D","F"));
         matrix_adj_3.put("C", Arrays.asList("A", "B", "C","D"));
@@ -36,7 +36,7 @@ public class Ciclo {
         matrix_adj_3.put("F", Arrays.asList("A", "E","G"));
         matrix_adj_3.put("G", Arrays.asList("A","D","E" ,"F"));
     }
-    public void setMatrix_adj_4(){
+    public static void setMatrix_adj_4(){
         matrix_adj_4.put("A", Arrays.asList("B", "C", "F", "G"));
         matrix_adj_4.put("B", Arrays.asList("A", "C","D","F"));
         matrix_adj_4.put("C", Arrays.asList("A", "B", "C","D"));
@@ -44,6 +44,45 @@ public class Ciclo {
         matrix_adj_4.put("E", Arrays.asList("C","D","F", "G"));
         matrix_adj_4.put("F", Arrays.asList("A","B", "E","G"));
         matrix_adj_4.put("G", Arrays.asList("A","D","E" ,"F"));
+    }
+
+    static {
+        setMatrix_adj_1();
+        setMatrix_adj_2();
+        setMatrix_adj_3();
+        setMatrix_adj_4();
+    }
+    private static boolean Dirac(Map<String, List<String>> graph) {
+        int n = graph.size();
+        if (n < 3) return false;
+        for (String vertex : graph.keySet()) {
+            int degree = graph.get(vertex).size();
+            if (degree < n / 2) return false;
+        }
+        return true;
+    }
+    private static boolean Ore(Map<String, List<String>> graph) {
+        int n = graph.size();
+        for (String u : graph.keySet()) {
+            for (String v: graph.keySet()){
+                if (!u.equals(v) && !graph.get(u).contains(v)){
+                    int degreeU = graph.get(u).size();
+                    int degreeV = graph.get(v).size();
+
+                    if (degreeU + degreeV < n){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean
+
+
+    public static void main(String[] args) {
+        Ore(matrix_adj_3);
     }
 }
 
