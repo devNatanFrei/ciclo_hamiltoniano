123d74813983dd2e59e0570de35535538e1e8c72
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-09-16 15:07:50 -0300

    organizei o codigo

--- Ciclo.java
+++ Ciclo.java
@@ -1,89 +1,88 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Ciclo {
 
-    private static final Map<String, List<String>> matrix_adj_1 = new HashMap<>();
-    private static final Map<String, List<String>> matrix_adj_2 = new HashMap<>();
-    private static final Map<String, List<String>> matrix_adj_3 = new HashMap<>();
-    private static final Map<String, List<String>> matrix_adj_4 = new HashMap<>();
+    private static final Map<String, List<String>> grafo1 = new HashMap<>();
+    private static final Map<String, List<String>> grafo2 = new HashMap<>();
+    private static final Map<String, List<String>> grafo3 = new HashMap<>();
+    private static final Map<String, List<String>> grafo4 = new HashMap<>();
 
     // Inicializa o grafo 1
-    public static void setMatrix_adj_1() {
-        matrix_adj_1.put("A", Arrays.asList("B", "C", "F", "G"));
-        matrix_adj_1.put("B", Arrays.asList("A", "C"));
-        matrix_adj_1.put("C", Arrays.asList("A", "B", "D"));
-        matrix_adj_1.put("D", Arrays.asList("C", "E"));
-        matrix_adj_1.put("E", Arrays.asList("D", "F"));
-        matrix_adj_1.put("F", Arrays.asList("A", "E", "G"));
-        matrix_adj_1.put("G", Arrays.asList("A", "F"));
+    public static void setGrafo1() {
+        grafo1.put("A", Arrays.asList("B", "C", "F", "G"));
+        grafo1.put("B", Arrays.asList("A", "C", "D", "F"));
+        grafo1.put("C", Arrays.asList("A", "B", "D", "E"));
+        grafo1.put("D", Arrays.asList("B", "C", "E", "G"));
+        grafo1.put("E", Arrays.asList("C", "D", "F", "G"));
+        grafo1.put("F", Arrays.asList("A", "B", "E", "G"));
+        grafo1.put("G", Arrays.asList("A", "D", "E", "F"));
     }
 
     // Inicializa o grafo 2
-    public static void setMatrix_adj_2() {
-        matrix_adj_2.put("A", Arrays.asList("B", "C", "F", "G"));
-        matrix_adj_2.put("B", Arrays.asList("A", "C"));
-        matrix_adj_2.put("C", Arrays.asList("A", "B", "D"));
-        matrix_adj_2.put("D", Arrays.asList("C", "E"));
-        matrix_adj_2.put("E", Arrays.asList("D", "F", "G"));
-        matrix_adj_2.put("F", Arrays.asList("A", "E", "G"));
-        matrix_adj_2.put("G", Arrays.asList("A", "E", "F"));
+    public static void setGrafo2() {
+        grafo2.put("A", Arrays.asList("B", "C", "F", "G"));
+        grafo2.put("B", Arrays.asList("A", "C", "D", "F"));
+        grafo2.put("C", Arrays.asList("A", "B", "D"));
+        grafo2.put("D", Arrays.asList("B", "C", "E", "G"));
+        grafo2.put("E", Arrays.asList("D", "F", "G"));
+        grafo2.put("F", Arrays.asList("A", "B", "E", "G"));
+        grafo2.put("G", Arrays.asList("A", "D", "E", "F"));
     }
 
     // Inicializa o grafo 3
-    public static void setMatrix_adj_3() {
-        matrix_adj_3.put("A", Arrays.asList("B", "C", "F", "G"));
-        matrix_adj_3.put("B", Arrays.asList("A", "C", "D", "F"));
-        matrix_adj_3.put("C", Arrays.asList("A", "B", "D"));
-        matrix_adj_3.put("D", Arrays.asList("B", "C", "E", "G"));
-        matrix_adj_3.put("E", Arrays.asList("D", "F", "G"));
-        matrix_adj_3.put("F", Arrays.asList("A", "E", "G"));
-        matrix_adj_3.put("G", Arrays.asList("A", "D", "E", "F"));
+    public static void setGrafo3() {
+        grafo3.put("A", Arrays.asList("B", "C", "F", "G"));
+        grafo3.put("B", Arrays.asList("A", "C"));
+        grafo3.put("C", Arrays.asList("A", "B", "D"));
+        grafo3.put("D", Arrays.asList("C", "E"));
+        grafo3.put("E", Arrays.asList("D", "F", "G"));
+        grafo3.put("F", Arrays.asList("A", "E", "G"));
+        grafo3.put("G", Arrays.asList("A", "E", "F"));
     }
 
     // Inicializa o grafo 4
-    public static void setMatrix_adj_4() {
-        matrix_adj_4.put("A", Arrays.asList("B", "C", "F", "G"));
-        matrix_adj_4.put("B", Arrays.asList("A", "C", "D", "F"));
-        matrix_adj_4.put("C", Arrays.asList("A", "B", "D"));
-        matrix_adj_4.put("D", Arrays.asList("B", "C", "E", "G"));
-        matrix_adj_4.put("E", Arrays.asList("C", "D", "F", "G"));
-        matrix_adj_4.put("F", Arrays.asList("A", "B", "E", "G"));
-        matrix_adj_4.put("G", Arrays.asList("A", "D", "E", "F"));
+    public static void setGrafo4() {
+        grafo4.put("A", Arrays.asList("B", "C", "F", "G"));
+        grafo4.put("B", Arrays.asList("A", "C"));
+        grafo4.put("C", Arrays.asList("A", "B", "D"));
+        grafo4.put("D", Arrays.asList("C", "E"));
+        grafo4.put("E", Arrays.asList("D", "F"));
+        grafo4.put("F", Arrays.asList("A", "E", "G"));
+        grafo4.put("G", Arrays.asList("A", "F"));
     }
-
 
     static {
-        setMatrix_adj_1();
-        setMatrix_adj_2();
-        setMatrix_adj_3();
-        setMatrix_adj_4();
+        setGrafo1();
+        setGrafo2();
+        setGrafo3();
+        setGrafo4();
     }
 
-
+    // Teorema de Dirac
     private static boolean Dirac(Map<String, List<String>> graph) {
         int n = graph.size();
-        if (n < 3) return false;
-        for (String vertex : graph.keySet()) {
-            int degree = graph.get(vertex).size();
-            if (degree < n / 2.0) return false;
+        if (n < 3) {
+            return false;
+        }
+
+        for (String v : graph.keySet()) {
+            if (graph.get(v).size() < n / 2) {
+                return false;
+            }
         }
         return true;
     }
 
 
+    // Teorema de Ore
     private static boolean Ore(Map<String, List<String>> graph) {
         int n = graph.size();
         for (String u : graph.keySet()) {
-            for (String v: graph.keySet()){
-                if (!u.equals(v) && !graph.get(u).contains(v)){
-                    int degreeU = graph.get(u).size();
-                    int degreeV = graph.get(v).size();
-                    if (degreeU + degreeV < n){
+            for (String v : graph.keySet()) {
+                if (!u.equals(v) && !graph.get(u).contains(v)) {
+                    int grauU = graph.get(u).size();
+                    int grauV = graph.get(v).size();
+                    if (grauU + grauV < n) {
                         return false;
                     }
                 }
@@ -92,7 +91,7 @@ public class Ciclo {
         return true;
     }
 
-
+    // Teorema de Bondy & Chvátal
     private static boolean BondyChvatal(Map<String, List<String>> graph) {
         Map<String, Set<String>> closure = new HashMap<>();
         for (String vertex : graph.keySet()) {
@@ -100,7 +99,7 @@ public class Ciclo {
         }
 
         boolean changed = true;
-        while (changed){
+        while (changed) {
             changed = false;
             for (String u : closure.keySet()) {
                 for (String v : closure.keySet()) {
@@ -119,7 +118,6 @@ public class Ciclo {
             }
         }
 
-
         for (String u : closure.keySet()) {
             for (String v : closure.keySet()) {
                 if (!u.equals(v) && !closure.get(u).contains(v)) {
@@ -131,27 +129,26 @@ public class Ciclo {
     }
 
     public static void main(String[] args) {
-
-        System.out.println("Dirac - Grafo 1: " + Dirac(matrix_adj_1));
-        System.out.println("Ore - Grafo 1: " + Ore(matrix_adj_1));
-        System.out.println("Bondy & Chvátal - Grafo 1: " + BondyChvatal(matrix_adj_1));
-
-        System.out.println();
-
-        System.out.println("Dirac - Grafo 2: " + Dirac(matrix_adj_2));
-        System.out.println("Ore - Grafo 2: " + Ore(matrix_adj_2));
-        System.out.println("Bondy & Chvátal - Grafo 2: " + BondyChvatal(matrix_adj_2));
+        System.out.println("Dirac - Grafo 1: " + Dirac(grafo1));
+        System.out.println("Ore - Grafo 1: " + Ore(grafo1));
+        System.out.println("Bondy & Chvátal - Grafo 1: " + BondyChvatal(grafo1));
 
         System.out.println();
 
-        System.out.println("Dirac - Grafo 3: " + Dirac(matrix_adj_3));
-        System.out.println("Ore - Grafo 3: " + Ore(matrix_adj_3));
-        System.out.println("Bondy & Chvátal - Grafo 3: " + BondyChvatal(matrix_adj_3));
+        System.out.println("Dirac - Grafo 2: " + Dirac(grafo2));
+        System.out.println("Ore - Grafo 2: " + Ore(grafo2));
+        System.out.println("Bondy & Chvátal - Grafo 2: " + BondyChvatal(grafo2));
 
         System.out.println();
 
-        System.out.println("Dirac - Grafo 4: " + Dirac(matrix_adj_4));
-        System.out.println("Ore - Grafo 4: " + Ore(matrix_adj_4));
-        System.out.println("Bondy & Chvátal - Grafo 4: " + BondyChvatal(matrix_adj_4));
+        System.out.println("Dirac - Grafo 3: " + Dirac(grafo3));
+        System.out.println("Ore - Grafo 3: " + Ore(grafo3));
+        System.out.println("Bondy & Chvátal - Grafo 3: " + BondyChvatal(grafo3));
+
+        System.out.println();
+
+        System.out.println("Dirac - Grafo 4: " + Dirac(grafo4));
+        System.out.println("Ore - Grafo 4: " + Ore(grafo4));
+        System.out.println("Bondy & Chvátal - Grafo 4: " + BondyChvatal(grafo4));
     }
 }
